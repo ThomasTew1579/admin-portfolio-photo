fdbaa8a6870d89dc6c6e1912fa09ed7c51166c8a
Thomas <pasquet.thomas69@gmail.com>
2025-08-29 23:03:51 +0200

    fix: style menu

--- admin/src/components/MenuItems.tsx
+++ admin/src/components/MenuItems.tsx
@@ -11,7 +11,7 @@ type MenuItemProps = {
   }
 
 function MenuItems({ children, icon = "arrow-right",  href, onClick }: MenuItemProps) {
-    const baseClass = 'px-3 pl-8.5 py-1 text-sm font-medium w-full text-start rounded-md text-white '
+    const baseClass = 'block px-3 pl-8.5 py-1.5 text-sm font-medium w-full text-start rounded-md text-white '
     const inactiveClass = 'opacity-90'
     const activeClass = 'bg-gray-300/60'
 
@@ -20,7 +20,7 @@ function MenuItems({ children, icon = "arrow-right",  href, onClick }: MenuItemP
         if (isExternal) {
             return (
                 <li className="list-none relative">
-                    <Icon name={icon} size={14} className="absolute top-1.5 left-3 fill-primary" />
+                    <Icon name={icon} size={14} className="absolute z-10 top-2 left-3 fill-primary" />
                     <a className={baseClass + inactiveClass} href={href} target="_blank" rel="noreferrer">
                         {children}
                     </a>
@@ -32,7 +32,7 @@ function MenuItems({ children, icon = "arrow-right",  href, onClick }: MenuItemP
             baseClass + (isActive ? activeClass : inactiveClass)
         return (
         <li className="list-none relative">
-            <Icon name={icon} size={14} className="absolute top-1.5 left-3 fill-primary" />
+            <Icon name={icon} size={14} className="absolute z-10 top-2 left-3 fill-primary" />
             <NavLink
                 to={href}
                 end={href === '/'}
@@ -45,7 +45,7 @@ function MenuItems({ children, icon = "arrow-right",  href, onClick }: MenuItemP
     }
     return (
         <li className="list-none relative">
-            <Icon name={icon} size={14} className="absolute top-1.5 left-3 fill-primary" />
+            <Icon name={icon} size={14} className="absolute z-10 top-2 left-3 fill-primary" />
             <button className={baseClass + inactiveClass} onClick={onClick}>
                 {children}
             </button>
