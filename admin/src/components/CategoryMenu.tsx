d84ca223c30a5f21275505bb682f1dd8ab46bee8
Thomas <pasquet.thomas69@gmail.com>
2025-09-05 00:24:18 +0200

    fix: nouvell couleur design system

--- admin/src/components/CategoryMenu.tsx
+++ admin/src/components/CategoryMenu.tsx
@@ -8,7 +8,7 @@ type CategoryMenuProps = {
 function CategoryMenu({ children, className, name }: CategoryMenuProps) {
   return (
     <div className={'pt-2.5 px-2 ' + className}>
-      <span className="text-xs font-medium pl-1 text-gray-400">{name}</span>
+      <span className="text-xs font-medium pl-1 text-gray-100">{name}</span>
       {children}
     </div>
   );
