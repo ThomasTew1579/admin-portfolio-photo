cc8c26688a2a5176573c576b7ff9acd0ae39b6e6
Thomas <pasquet.thomas69@gmail.com>
2025-09-04 09:18:53 +0200

    fix: changement fonctionnement grid photoLibrary

--- admin/src/pages/PhotoLibrary.tsx
+++ admin/src/pages/PhotoLibrary.tsx
@@ -1,4 +1,5 @@
 import gallery from '../assets/gallery.json'
+import Icon from '../components/Icon';
 
 
 // type Photo = {
@@ -22,27 +23,23 @@ type PhotoLibraryProps = {
 
 
 function PhotoLibrary({grid , objectFit}: PhotoLibraryProps) {
-    
 
-    var fit: string = objectFit ? "object-cover": "object-contain";
+
+    var fit: string = objectFit ? " md:object-cover": " md:object-contain";
 
     return (
         <div className="photo-library p-3">
             <div 
-            // className={`grid ${gap} ${cols}` }
-            className="
-        grid gap-3 duration-200
-        [grid-template-columns:repeat(auto-fit,minmax(var(--grid,220px),1fr))]
-      "
-      style={{ ['--grid' as any]: `${grid}px` }}
-            
+            className="grid gap-2 grid-cols-3 md:[grid-template-columns:repeat(auto-fit,minmax(var(--grid,220px),1fr))] "
+            style={{ ['--grid' as any]: `${grid}px` }}
             >
                 {gallery.map((_, idx) => (
-                    <button key={idx} className="photo aspect-square h-full overflow-clip">
-                        <img className={"h-full w-full " + fit} src={gallery[idx].path} alt=""  
-                        
-                        />
-                    </button>
+                    <div key={idx} className="photo relative aspect-square h-full overflow-clip group">
+                        <img className={"h-full w-full object-cover group-hover:scale-110 duration-200 group-hover:opacity-80" + fit} src={gallery[idx].path} alt=""  />
+                        <button className="absolute top-1/2 left-1/2 -translate-1/2 group-hover:opacity-100 opacity-0 duration-200">
+                            <Icon name="circle-ellipsis" size={40} className="fill-white z-10" />
+                        </button>
+                    </div>
                 ))}
             </div>
         </div>
