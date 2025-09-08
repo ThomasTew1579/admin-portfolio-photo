72c1c83a021907889b3eec0ab135d18d10ee9de3
Thomas <pasquet.thomas69@gmail.com>
2025-09-08 14:38:08 +0200

    perf: lazy load des pages

--- admin/src/Admin.tsx
+++ admin/src/Admin.tsx
@@ -1,13 +1,15 @@
 import { Routes, Route } from 'react-router-dom';
 import type { ViewportConfig } from './types/type';
+import { Suspense, lazy } from 'react';
 
 import SideMenu from './components/SideMenu';
 import TopMenu from './components/TopMenu';
-import PhotoLibrary from './pages/PhotoLibrary';
-import PhotoUploadForm from './pages/PhotoUploadForm';
-import Albums from './pages/Albums';
-import AlbumSingle from './pages/AlbumSingle';
-import TagSingle from './pages/TagSingle';
+const PhotoLibrary = lazy(() => import('./pages/PhotoLibrary'));
+const PhotoUploadForm = lazy(() => import('./pages/PhotoUploadForm'));
+const Albums = lazy(() => import('./pages/Albums'));
+const AlbumSingle = lazy(() => import('./pages/AlbumSingle'));
+const TagSingle = lazy(() => import('./pages/TagSingle'));
+
 import { useState } from 'react';
 
 function Admin() {
@@ -21,6 +23,10 @@ function Admin() {
 
   const changeGrid = (n: number) => setViewportConfig((prev) => ({ ...prev, grid: n }));
 
+  function Fallback() {
+    return <div className="p-6 text-center text-white">Chargement…</div>;
+  }
+
   return (
     <>
       <SideMenu />
@@ -32,26 +38,30 @@ function Admin() {
       />
 
       <div className="viewport pl-sidebar pt-topbar">
-        <Routes>
-          <Route
-            path="/"
-            element={
-              <PhotoLibrary grid={viewportConfig.grid} objectFit={viewportConfig.objectFit} />
-            }
-          />
-          <Route path="/upload" element={<PhotoUploadForm />} />
-          <Route path="/albums" element={<Albums />} />
-          <Route
-            path="/album"
-            element={
-              <AlbumSingle grid={viewportConfig.grid} objectFit={viewportConfig.objectFit} />
-            }
-          />
-          <Route
-            path="/tag"
-            element={<TagSingle grid={viewportConfig.grid} objectFit={viewportConfig.objectFit} />}
-          />
-        </Routes>
+        <Suspense fallback={<Fallback />}>
+          <Routes>
+            <Route
+              path="/"
+              element={
+                <PhotoLibrary grid={viewportConfig.grid} objectFit={viewportConfig.objectFit} />
+              }
+            />
+            <Route path="/upload" element={<PhotoUploadForm />} />
+            <Route path="/albums" element={<Albums />} />
+            <Route
+              path="/album"
+              element={
+                <AlbumSingle grid={viewportConfig.grid} objectFit={viewportConfig.objectFit} />
+              }
+            />
+            <Route
+              path="/tag"
+              element={
+                <TagSingle grid={viewportConfig.grid} objectFit={viewportConfig.objectFit} />
+              }
+            />
+          </Routes>
+        </Suspense>
       </div>
     </>
   );
